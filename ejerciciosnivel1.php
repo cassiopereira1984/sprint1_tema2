b7e5bf50b34e521b84d083bfbe65b9239943c7d6
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-02-14 13:25:19 +0100

    php ejercicio2 + strtoupper + strlen

--- ejerciciosnivel1.php
+++ ejerciciosnivel1.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 
-// Defineix una variable de cada tipus: integer, double, string i boolean. Imprimeix-les per pantalla.
+// EJERCICIO1 Defineix una variable de cada tipus: integer, double, string i boolean. Imprimeix-les per pantalla.
 integer
 double 
 string 
@@ -16,11 +16,20 @@ define("nombre", "Cassio");
 
 echo "Mi nombre es" . nombre ", gracias.";
 
-//Imprimeix per pantalla "Hello, World!" utilitzant una variable
-define("Eje", "Hello World");
-echo Eje;
+//EJERCICIO 2 Imprimeix per pantalla "Hello, World!" utilitzant una variable
+$hello = "Hello World";
+echo $hello;
 
+//Transforma tots els caràcters de l'string a majúscules i imprimeix en pantalla.
+echo strtoupper($hello);
 
+//Imprimeix per pantalla la mida (longitud) de la variable
+echo strlen($hello);
+
+/*EJERCICIO 3 Declara dues variables X i Y de tipus int, dues variables N i M de tipus double i 
+assigna a cadascuna un valor. A continuació, mostra per pantalla per a X i Y:*/
+integer X, Y = 4, 5;
+double N, M = 6, 7;
 
 
 ?>
