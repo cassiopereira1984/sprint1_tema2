7eb868db642ac48b815d32023d1b898f9146e648
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-02-14 12:05:18 +0100

    php ejercicio1

---
+++ ejerciciosnivel1.php
@@ -0,0 +1,16 @@
+<html>
+<header>
+    <title>Sprint1 Tema2</title>
+</header>
+<body>
+<?php
+
+// Defineix una variable de cada tipus: integer, double, string i boolean. Imprimeix-les per pantalla.
+$nombre = "Cassio"
+
+echo "Mi nombre es" . $nombre ", gracias. "\n";
+
+?>
+
+</body>
+</html>
